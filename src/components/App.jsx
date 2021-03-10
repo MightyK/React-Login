cd474ecba200106d82b705346e551f785c917e18
MightyK <KMightyDev@gmail.com>
2021-03-09 20:15:10 -0500

    Final default configurations

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="container">
+            <h1>Hello</h1>
+            <form className="form">
+                <input type="text" placeholder="Username" />
+                <input type="text" placeholder="Password" />
+                <button type="submit">Login</button>
+            </form>
+        </div>
+    );
+};
+
+export default App;
